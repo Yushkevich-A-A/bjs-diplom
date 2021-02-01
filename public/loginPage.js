3049a34a2c74cf6522e4cbe69c6e78203508cf78
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-02-01 09:56:35 +0300

    autorisation added

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,2 +1,14 @@
 'use strict'
 
+const userForm = new UserForm();
+
+//аутентификация при входе на сайт
+
+userForm.loginFormCallback = data => ApiConnector.login(data, response => {
+    if (response.success){
+        location.reload();
+    } else {
+        userForm.setLoginErrorMessage(response.error);
+    }
+});
+
