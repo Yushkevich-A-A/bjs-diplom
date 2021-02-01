cf31604edef036b091c841f00ef06c33f51aed8d
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-02-01 16:35:59 +0300

    modules of get stocks, transfer money added

---
+++ public/homePage.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const logoutButton = new LogoutButton();
+const ratesBoard = new RatesBoard();
+const moneyManager = new MoneyManager();
+const favoritesWidget = new FavoritesWidjet();
+const minute = 60000;
+
+let messageSuccessfulDepositOfBalance = 'Пополнение баланса выполнено успешно';
+let messageSuccessfulConvertationOfValuence = 'Конвертация валюты выполнена успешно';
+let messageSuccessfulTransferOfValuence = 'Отправка денег другому пользователю выполнена успешно';
+
+// функция вывода сообщения об успехе или ошибке и обновления данных
+
+function managerMessagesAndReloadModule (objectData, message) {
+    if (objectData.success) {
+        setTimeout(() => resetCurrentUserDate(), 500);
+        moneyManager.setMessage(objectData.success, message)
+    } else {
+        moneyManager.setMessage(objectData.success, objectData.error);
+    }
+}
+
+// Выход из учетной записи по кнопке выход
+
+logoutButton.action = () => ApiConnector.logout(() => location.reload());
+
+// Отображение данных профиля
+
+let resetCurrentUserDate = () => {
+    ApiConnector.current(objectProfileData => {
+        ProfileWidget.showProfile(objectProfileData.data)
+    });
+};
+resetCurrentUserDate();
+
+// Обновление и отображение курса валют
+
+ApiConnector.getStocks(objectData => {
+    const getRates = () => {
+        ratesBoard.clearTable();
+        ratesBoard.fillTable(objectData.data);
+    };
+    getRates();
+    setInterval(() => getRates(), minute);
+});
+
+// Операции с деньгами, пополнение баланса ??? непонятно почему после запроса внесения суммы на счет 
+// для оторажения информации должно пройти 500-1000мс
+
+moneyManager.addMoneyCallback = data => {
+    ApiConnector.addMoney(data, objectData => {
+        managerMessagesAndReloadModule(objectData, messageSuccessfulDepositOfBalance)
+    });
+};
+
+// Конвертация валюты
+
+moneyManager.conversionMoneyCallback = data => {
+    ApiConnector.convertMoney(data, objectData => {
+        managerMessagesAndReloadModule(objectData, messageSuccessfulConvertationOfValuence)
+    });
+};
+
+// Перевод валюты между пользователями
+
+moneyManager.sendMoneyCallback = data => {
+    ApiConnector.transferMoney(data, objectData => {
+        managerMessagesAndReloadModule(objectData, messageSuccessfulTransferOfValuence)
+    });
+};
+
+// 
